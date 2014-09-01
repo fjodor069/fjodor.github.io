515b0f7695e7cb7074e668ce3cc720beaa05d3eb
Fjodor069 <napoleon.001@hetnet.nl>
2014-09-01 21:35:28 +0200

    Jelly plugin
    source for minecraft plugin

---
+++ Jelly/IgniteCommand.java
@@ -0,0 +1,55 @@
+package nl.Jelly;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+//
+//this class is handling the /ignite command
+//
+//usage /ignite <player> <seconds> : set a player on fire
+//
+// note: dit werkt alleen in SURVIVAL of ADVENTURE mode
+// met dank aan: Essentials
+//
+public class IgniteCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (args.length < 1)
+			return false;
+
+		// check if the command was issued by a player
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("This command requires a player!");
+			return true;
+		}
+
+		// get player name from command parameter
+		Player target = (Player) sender.getServer().getPlayer(args[0]);
+
+		// make sure this player is online
+		if (target == null) {
+			sender.sendMessage(args[0] + " is not currently online.");
+			return true;
+		}
+
+		int seconds;
+		if (args.length == 2) {
+			seconds = Integer.parseInt(args[1]);
+
+		} else {
+			// set player on fire for default 200 ticks (20 tps so 10 sec)
+			seconds = 10;
+		}
+
+		target.setFireTicks(seconds * 20);
+		target.sendMessage(String.format("You were ignited by %s ",
+				sender.getName()));
+
+		return true;
+	}
+
+}
---
+++ Jelly/SignCache.java
@@ -0,0 +1,124 @@
+package nl.Jelly;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.block.Sign;
+
+///
+// this class keeps track of known signs and their lift status
+// the signs details are processed once and kept in a hashmap
+// when a lift sign is placed or destroyed, iterate through
+// the existing lifts to find possible matches
+//
+public class SignCache 
+{
+	private final Map<String, SignDetail> signs = new HashMap<String, SignDetail>();
+	
+	
+	
+	public SignCache()
+	{
+		
+	}
+	
+	//return the hashmap object to debug
+	public HashMap<String,SignDetail> getMap()
+	{
+		return (HashMap<String,SignDetail>)signs;
+	}
+	
+	public SignDetail getCachedSignDetail(Location location)
+	{
+		final String locationString = getLocationKey(location);
+		
+		
+		return signs.get(locationString);
+	}
+
+	
+	//to be called when a new sign is created or an existing sign is noticed in game
+	//and needs to be added to the cache
+	public SignDetail newSignCreated(Sign sign)
+	{
+		//check if there is a signDetail object at this location
+		SignDetail signDetail = getCachedSignDetail(sign.getLocation());
+		
+		if (signDetail != null)
+		{
+			//if we overwrite an existing signdetail
+			//remove it first
+			invalidateCacheLocation(signDetail);
+			signs.remove(getLocationKey(signDetail));
+		}
+		
+		
+		//create a signdetail and add it to the cache
+		signDetail = new SignDetail(sign,null);
+		signs.put(getLocationKey(signDetail), signDetail);
+		invalidateCacheLocation(signDetail);
+		
+		return signDetail;
+	}
+	
+	//the same but now the SignDetail object is already created
+	public SignDetail newSignCreated(SignDetail signDetail)
+	{
+		//check if there is a signDetail object in the cache
+		SignDetail cached = signs.get(getLocationKey(signDetail));
+		
+		
+		if (cached != null)
+		{
+			//the object is in the cache
+			invalidateCacheLocation(cached);
+			signs.remove(getLocationKey(cached));
+		}
+		
+		signs.put(getLocationKey(signDetail), signDetail);
+		invalidateCacheLocation(signDetail);
+		
+		return signDetail;
+	}
+	
+	
+	public void existingSignDestroyed(Sign sign)
+	{
+		SignDetail signDetail = getCachedSignDetail(sign.getLocation());
+		
+		if (signDetail != null)
+		{
+			signs.remove(getLocationKey(signDetail));
+			if (signDetail.isLiftSign())
+			{
+				invalidateCacheLocation(signDetail);
+			}
+		}
+	}
+	
+	private void invalidateCacheLocation(SignDetail signDetail)
+	{
+		for (SignDetail val : signs.values())
+		{
+			val.clearCache(signDetail);
+		
+		}
+	}
+	
+	private String getLocationKey(final Location l)
+	{
+		return l.getWorld().getName() + "," + l.getBlockX() + ","  + l.getBlockY() + "," + l.getBlockZ();
+		
+	
+	}
+	
+	private String getLocationKey(final SignDetail signDetail)
+	{
+		return getLocationKey(signDetail.getLocation());
+		
+	
+	}
+	
+	
+}
